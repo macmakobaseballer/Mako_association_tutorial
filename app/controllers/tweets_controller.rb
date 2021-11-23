8678b97e389c476cf0ec5f4638a8e3acd18a0c91
EC2 Default User <ec2-user@ip-10-0-1-146.ap-northeast-1.compute.internal>
2021-11-23 02:01:14 +0000

    CRUDの実装：controllerの作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,13 +1,16 @@
 class TweetsController < ApplicationController
   def new
+    @tweet = Tweet.new
   end
 
   def index
+    @tweets = Tweet.all
   end
 
   def show
   end
 
   def create
+    @tweet = Tweet.find(params[:id])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
+  before_acton :authenticate_user!
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 end
