c843d6fc469015770bead58077d217784b66096e
EC2 Default User <ec2-user@ip-10-0-1-146.ap-northeast-1.compute.internal>
2021-11-29 13:51:12 +0000

    CRUDの実装：フォロワー機能の実装・アソシエーションの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,26 @@ class User < ApplicationRecord
   has_many :tweets
   has_many :favorites
   has_many :comments
+  
   has_many :favorite_tweets, through: :favorites, source: :tweet
   has_many :comment_tweets, through: :comments, source: :tweet
+  
+  #======= 自分がフォローするユーザーとのリレーション========
+  #フォローする側のUserから見て、フォローされる側のUserを(中間テーブルを介して)集める。なので親はfollowing_id(フォローする側)
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+  # 中間テーブルを介して「follower」モデルのUser(フォローされた側)を集めることを「followings」と定義
+  has_many :followings, through: :active_relationships, source: :follower
+  #===========================================================
+  
+  #======= 自分がフォローされるユーザーとのリレーション=======
+  #フォローされる側のUserから見て、フォローしてくる側のUserを(中間テーブルを介して)集める。なので親はfollower_id(フォローされる側)
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  # 中間テーブルを介して「following」モデルのUser(フォローする側)を集めることを「followers」と定義
+  has_many :followers, through: :passive_relationships, source: :following
+  #===========================================================
+  
+  def followed_by?(user)
+    passive_relationships.find_by(following_id: user.id).present?
+  end
   
 end
