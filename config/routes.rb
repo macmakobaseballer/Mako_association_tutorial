7e37e410b37d237623391d8dcfd029a207205d4c
EC2 Default User <ec2-user@ip-10-0-1-146.ap-northeast-1.compute.internal>
2021-11-29 13:20:23 +0000

    CRUDの実装：フォロー機能の実装・ルーティングの定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  # get 'relationships/create'
+  # get 'relationships/destroy'
   # get 'favorites/create'
   # get 'favorites/destroy'
   root 'tweets#index'
@@ -13,10 +15,17 @@ Rails.application.routes.draw do
   
   
   devise_for :users
+  
+  
   resources :tweets do
     resource :favorites, only: [:create, :destroy]  
-    resource :comments, only: :create
+    resource :comments, only: [:create]
   end
-  resources :users
+  
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get :follows, on: :member
+    get :followers, on: :member
+  end  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
