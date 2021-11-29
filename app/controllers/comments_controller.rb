a9e2ba1e9e7ce806dbf13eca31e210c69b536ec8
EC2 Default User <ec2-user@ip-10-0-1-146.ap-northeast-1.compute.internal>
2021-11-29 12:53:56 +0000

    CRUDの実装：コメント機能の実装・画面のフィックス完了

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+  def create
+    tweet = Tweet.find(params[:tweet_id])
+    comment = current_user.comments.build(comment_params)
+    comment.tweet_id = tweet.id
+    comment.save
+    redirect_to tweets_path
+  end
+  
+  private 
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
 end
